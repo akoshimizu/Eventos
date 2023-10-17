2537a48bc620c1474803972faf2a975fa9c61101
akoshimizu <anderson_koshimizu@hotmail.com>
2023-10-17 14:14:15 -0300

    Add Componts User/Login/Registration, SubComponents e suas rotas + Ajustes

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -13,17 +13,22 @@ import { NgxSpinnerModule } from 'ngx-spinner';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
+import { NavComponent } from './shared/nav/nav.component';
 import { EventosComponent } from './components/eventos/eventos.component';
 import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
 import { ContatosComponent } from './components/contatos/contatos.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { PerfilComponent } from './components/perfil/perfil.component';
+import { PerfilComponent } from './components/user/perfil/perfil.component';
 import { TituloComponent } from './shared/titulo/titulo.component';
 
 import { EventoService } from './services/evento.service';
 
 import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
+import { EventoDetalheComponent } from './components/eventos/evento-detalhe/evento-detalhe.component';
+import { EventoListaComponent } from './components/eventos/evento-lista/evento-lista.component';
+import { UserComponent } from './components/user/user.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegistrationComponent } from './components/user/registration/registration.component';
 
 
 @NgModule({
@@ -36,7 +41,12 @@ import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
       PerfilComponent,
       NavComponent,
       DateTimeFormatPipe,
-      TituloComponent
+      TituloComponent,
+      EventoDetalheComponent,
+      EventoListaComponent,
+      UserComponent,
+      LoginComponent,
+      RegistrationComponent
    ],
   imports: [
     BrowserModule,
--- Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
@@ -1,13 +1,5 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
 
-import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
-import { ToastrService } from 'ngx-toastr';
-import { NgxSpinnerService } from 'ngx-spinner';
-
-import { EventoService } from '../../services/evento.service';
-import { Evento } from '../../models/Evento';
-
-
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
@@ -15,79 +7,9 @@ import { Evento } from '../../models/Evento';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: Evento[] =[];
-  public eventosFiltrados: Evento[] = [];
+  constructor(){ }
 
-  public larguraImagem: number = 80;
-  public margemImagem: number = 2;
-  public mostrarImagem: boolean = true;
-  private _filtroLista: string = '';
+  ngOnInit(): void {
 
-  public get filtroLista(): string {
-    return this._filtroLista;
-  }
-
-  public set filtroLista(value: string) {
-    this._filtroLista = value;
-    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
-  }
-
-  public filtrarEventos(filtrarPor: string): Evento[] {
-    filtrarPor = filtrarPor.toLocaleLowerCase();
-    return this.eventos.filter(
-      (evento: any)  => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
-                        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
-    )
-  }
-
-  constructor(
-    private eventoService: EventoService,
-    private modalService: BsModalService,
-    private toastr: ToastrService,
-    private spinner: NgxSpinnerService
-    ) { }
-
-    modalRef?: BsModalRef;
-  public ngOnInit() {
-    this.spinner.show();
-    this.getEventos();
-  }
-
-  public alterarImagem(): void {
-    this.mostrarImagem = !this.mostrarImagem;
-  }
-
-  public getEventos(): void {
-        this.eventoService.getEventos().subscribe({
-        next: (_eventos: Evento[]) => {
-          this.eventos = _eventos,
-          this.eventosFiltrados = this.eventos
-        },
-        error: (error: any) => {
-          this.spinner.hide();
-          this.toastr.error('Erro ao carregar eventos.', 'Erro!');
-        },
-        complete: () => this.spinner.hide()
-      });
-
-      // (_eventos: Evento[]) => {
-      //   this.eventos = _eventos,
-      //   this.eventosFiltrados = this.eventos
-      // },
-      // error => console.log(error)
-    // );
-  }
-
-  openModal(template: TemplateRef<any>): void {
-    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
-  }
-
-  confirm(): void {
-    this.modalRef?.hide();
-    this.toastr.success('Evento deletado com sucesso.', 'Deletado!');
-  }
-
-  decline(): void {
-    this.modalRef?.hide();
   }
 }
